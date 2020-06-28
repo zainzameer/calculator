fedd7cfc40ee1ac7eb01481777827bc394c0fe52
zain bin zameer <zainbinzameer@gmail.com>
2020-06-27 20:31:38 -1200

    calculator

---
+++ public/app.js
@@ -0,0 +1,12 @@
+function number(num){
+    var result=document.getElementById("result");
+    result.value += num;
+}
+function clearResult(){
+    var result= document.getElementById("result");
+    result.value=""
+}
+function getResult(){
+    var result= document.getElementById("result");
+    result.value= eval(result.value)
+}
